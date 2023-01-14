eb5f6b1ead1cea7bfa91e3c0e8a54879d8c0a12f
Ekrem <ekrem.karali@bilgiedu.net>
2023-01-14 23:09:02 +0300

    Just before placing rooms

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -10,3 +10,18 @@ public enum Orientation
     west,
     none
 }
+
+public enum GameState
+{
+    gameStarted,
+    playingLevel,
+    engagingEnemies,
+    bossStage,
+    engagingBoss,
+    levelCompleted,
+    gameWon,
+    gameLost,
+    gamePaused,
+    dungeonOverviewMap,
+    restartGame
+}
