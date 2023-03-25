82979bd299cb2bc6cd234d32b7f17f5e07ead5fa
Ekrem <ekrem.karali@bilgiedu.net>
2023-03-25 07:24:11 +0300

    Instantiating player to the dungeon done

--- Assets/Scripts/Dungeon/DungeonBuilder.cs
+++ Assets/Scripts/Dungeon/DungeonBuilder.cs
@@ -485,6 +485,9 @@ public class DungeonBuilder : SingletonMonobehaviour<DungeonBuilder>
         {
             room.parentRoomID = "";
             room.isPreviouslyVisited = true;
+            
+            // Set entrance in game manager
+            GameManager.Instance.SetCurrentRoom(room);
         }
         else
         {
